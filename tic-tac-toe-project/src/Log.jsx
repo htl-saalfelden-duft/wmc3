0407f3530b01bdcd28b1f62847e5e10f4aeda28e
dduft <dietmar@duft.it>
2024-02-26 15:23:36 +0100

    Changes from 06.02

---
+++ tic-tac-toe-project/src/Log.jsx
@@ -0,0 +1,3 @@
+export const Log = () => {
+    return (<h1>Hallo Welt</h1>)
+} 
--- tic-tac-toe-project/src/Player.jsx
+++ tic-tac-toe-project/src/Player.jsx
@@ -1,26 +1,31 @@
 import { useState } from "react"
 
-export const Player = ({name, symbol}) => {
+export const Player = ({initialName, symbol}) => {
 
+    const [ playerName, setPlayerName ] = useState(initialName);
     const [ isEditing, setIsEditing ] = useState(false)
 
     const handleEditClick = () => {
-        setIsEditing(true)
+        setIsEditing((isEditing) => !isEditing)
     }
 
-    let playerName = <span className="player-name">{name}</span>
+    const handleChange = (event) => {
+        setPlayerName(event.target.value)
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
 
     if(isEditing) {
-        playerName = <input type="text" required />
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
