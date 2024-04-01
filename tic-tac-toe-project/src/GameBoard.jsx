7514cb61be924b0759de8e1014b85ad1bb013741
dduft <dietmar@duft.it>
2024-04-01 15:23:47 +0200

    Till 90

--- tic-tac-toe-project/src/GameBoard.jsx
+++ tic-tac-toe-project/src/GameBoard.jsx
@@ -1,42 +1,13 @@
-import { useState } from "react"
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-]
-
-export const GameBoard = ({onSelectSquare, turns}) => {
-    let gameBoard = initialGameBoard
-
-    for(const turn of turns) {
-        const {square, player} = turn
-        const {row, col} = square
-
-        gameBoard[row][col] = player
-    }
-
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard)
-
-    // const handleSelectSquare = (rowIndex, colIndex) => {
-    //     setGameBoard((prevGameBoard) => {
-    //         const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-    //         updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol
-            
-    //         return updatedGameBoard
-    //     })
-
-    //     onSelectSquare()
-    // }
-
+export const GameBoard = ({onSelectSquare, board}) => {
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol != null}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
